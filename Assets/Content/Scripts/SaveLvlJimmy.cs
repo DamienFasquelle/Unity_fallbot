15c1ea07dd0c9e0458e03521cfee00f5315a7534
Jimmy <jimmy.carlier.mma@gmail.com>
2026-02-18 15:33:33 +0100

    Commit changes

---
+++ Assets/Content/Scripts/SaveLvlJimmy.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class SaveLvlJimmy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Checkpoint");
+    }
+}
