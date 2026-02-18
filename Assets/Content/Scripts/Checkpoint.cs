2d6ddcd9158a6820950026b522cf57cd9ea69999
Damdamdev <damen.fasquelle@lapiscine.pro>
2026-02-18 15:12:45 +0100

    first test git

---
+++ Assets/Content/Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerRespawn player = other.GetComponent<PlayerRespawn>();
+            if (player != null)
+            {
+                player.SetCheckpoint(transform.position);
+            }
+        }
+    }
+}
